9bbcab32dd026c83afca075a8d0bd8f85dc7e326
lfv-1 <nega123497@gmail.com>
2022-01-12 03:07:40 +0300

    feat: completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+    let a = ['','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen','eighteen','nineteen'];
+    let b = ['', '', 'twenty', 'thirty', 'forty','fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let hundreds = Math.trunc(number/100);
+    let tens = Math.trunc(number/10);
+    let ones = number % 10;
+    if (number === 0) {
+      return 'zero';
+  }else if (number < 20) {
+    return a[number];
+  }
+  else if(number <= 99) {
+    if (number % 10 == 0){
+        return `${b[tens]}`;
+    }else {
+    return `${b[tens]} ${a[ones]}`;
+  }
 }
+  else if(number < 1000 ) {
+      if (number % 100 < 20){
+          if (number % 100 == 0){
+        return (`${a[hundreds]} hundred`);
+      }else 
+        return (`${a[hundreds]} hundred ${a[Math.trunc(number % 100)]}`);
+      }else if (number % 10 == 0){
+        return (`${a[hundreds]} hundred ${b[Math.trunc(number % 100/10)]}`);
+      }else
+        return(`${a[hundreds]} hundred ${b[Math.trunc(number % 100/10)]} ${a[ones]}`);
+    }
+}
+
